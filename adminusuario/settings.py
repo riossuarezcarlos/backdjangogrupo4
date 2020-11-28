a8a51b73702a58d15cae2ae444c3732e1a7f18fa
Carlos Rios <riossuarezcarlos@gmail.com>
2020-11-27 19:58:41 -0500

    Configurar Swagger

--- adminusuario/settings.py
+++ adminusuario/settings.py
@@ -155,5 +155,5 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 
  # Configure Django App for Heroku.
-import django_heroku
-django_heroku.settings(locals())
+ import django_heroku
+ django_heroku.settings(locals())
