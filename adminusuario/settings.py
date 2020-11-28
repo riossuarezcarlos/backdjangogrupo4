294dbd327e3ba55e782b4466ee79a8cd38c64c4a
Carlos Rios <riossuarezcarlos@gmail.com>
2020-11-27 19:45:16 -0500

    Configurar Swagger

--- adminusuario/settings.py
+++ adminusuario/settings.py
@@ -12,11 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
-
-# Configure Django App for Heroku.
-import django_heroku
-django_heroku.settings(locals())
-
+ 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -158,4 +154,6 @@ SIMPLE_JWT = {
 CORS_ALLOW_ALL_ORIGINS = True
 
 
- 
+ # Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
